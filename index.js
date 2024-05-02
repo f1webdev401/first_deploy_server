8d6956327fcad731b36ac2325b04ad475dc85f1f
adarog999 <adarog999@gmail.com>
2024-05-02 19:24:08 +0800

    origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 5000
 
 
 app.use(cors({
-     origin: 'http://localhost:3000',
+     origin: ['http://localhost:3000','https://f1webdev.netlify.app/'],
      credentials: true
 }))
 app.use(cookieParser())
